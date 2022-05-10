c5f0d282cb5d2d6a82e31bc49fdc7cf6a5f5a37a
Bruno Ricardo Correa <correaricardobruno@gmail.com>
2022-05-10 15:14:43 -0300

    Finishing project 3

--- trab03/main.c
+++ trab03/main.c
@@ -2,9 +2,24 @@
 
 #define SIZE 20
 
+void limparVetor(int* vetor) {
+    for(int i = 0; i < SIZE; i++) {
+        vetor[i] = -1;
+    }
+}
+
+int existe(int *rota, int cidade) {
+    for(int i = 0; (i < SIZE) && (rota[i] != -1); i++) {
+        if(rota[i] == cidade) return 1;
+    }
+    
+    return 0;
+}
+
 int main() {
-    int qtdNos, grafo[SIZE][SIZE], partida, chegada, distancia = 0, qtdCidades = 0, rota[SIZE], cidade = -1;
+    int qtdNos, grafo[SIZE][SIZE], partida, chegada, distancia = 0, qtdCidades = 1, rota[SIZE], cidade = -1;
     scanf("%d", &qtdNos); //recebe a quantidade de cidades
+    limparVetor(rota);
 
     for(int i = 0; i < qtdNos; i++) {
         for(int j = 0; j < qtdNos; j++) {
@@ -13,33 +28,30 @@ int main() {
     }
     scanf("%d", &partida); // armazena o ponto de saida
     scanf("%d", &chegada); // armazena o ponto de chegada
-    //rota[0] = partida;
+    rota[0] = partida;
 
-    for(int i = 0; i < qtdNos; i++) {
+    for(int i = partida; i < qtdNos; i = cidade) {
         int min = 0;
-        for(int j = 0; j < qtdNos; j++) {
-            if (((grafo[i][j] < min) || (min == 0)) && (grafo[i][j] != 0)) {
-                if (rota[qtdCidades-1] != grafo[i][j]) {
+        for(int j = 0; j < qtdNos;j++) {
+            if((grafo[i][j] != 0) && ((grafo[i][j] < min) || (min == 0)) && !existe(rota, j)) {
                     min = grafo[i][j];
                     cidade = j;
-                }
             }
         }
         distancia += min;
         rota[qtdCidades] = cidade;
-        i = cidade;
         qtdCidades++;
 
-        if(cidade == chegada) {
-            break;
-        }
+        if (chegada == cidade) break;
     }
 
     printf("Distancia total entre %d e %d: %d\n", partida, chegada, distancia);
     printf("Caminho: ");
-    for(int i = 0; i < qtdCidades; i++) {
+    for(int i = 0; i < qtdCidades; i++) 
+    {
         if(i+1 != qtdCidades) printf("%d->", rota[i]);
         else printf("%d\n", rota[i]);
     }
+
     return 0;
 }
